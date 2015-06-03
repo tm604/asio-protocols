84964a22f37e45e82da2cb22d02a0ad185b3ffe0
Tom Molesworth <tom@perlsite.co.uk>
2015-06-03 07:55:25 +0100

    ODR fix

--- src/amqp.cpp
+++ src/amqp.cpp
@@ -1,2 +1,20 @@
 #include <net/amqp.h>
 
+namespace std {
+
+string to_string(const net::amqp::connection_details &details)
+{
+	string s = details.protocol() + "://";
+	if(details.have_auth()) {
+		s += details.username() + ":" + details.password() + "@";
+	}
+	s += details.host();
+	if(details.have_port()) {
+		s += ":" + to_string(details.port());
+	}
+	s += "/" + details.vhost();
+	return s;
+}
+
+};
+
