8568f4262ea85b6549b8d502fca8269a3c288143
Tom Molesworth <tom@perlsite.co.uk>
2015-06-03 07:54:40 +0100

    Top-level include now just pulls in the client+connection

--- include/net/amqp/client.h
+++ include/net/amqp/client.h
@@ -2,6 +2,7 @@
 
 #include <boost/regex.hpp>
 
+#include <amqpcpp/amqpcpp.h>
 #include <net/amqp.h>
 #include <cps/future.h>
 
@@ -11,42 +12,54 @@ namespace amqp {
 class connection_details {
 public:
 	connection_details(
-	):protocol_{ "amqp" },
-	  host_{ "localhost" },
-	  port_{ 5672 },
-	  user_{ "guest" },
-	  pass_{ "guest" },
-	  vhost_{ "/" }
+		const std::string &host = "localhost",
+		const std::string &user = "guest",
+		const std::string &pass = "guest",
+		const std::string &vhost = "/",
+		const uint16_t port = 5672,
+		const std::string &proto = "amqp"
+	):protocol_{ proto },
+	  host_{ host },
+	  port_{ port },
+	  user_{ user },
+	  pass_{ pass },
+	  vhost_{ vhost }
 	{
 	}
 
-	bool
-	from_uri(const std::string &uri)
+	static
+	connection_details
+	parse(const std::string &uri)
 	{
 		static const boost::regex re(
 			"^(?<protocol>amqps?)://(?:(?<user>[^:]*):(?<password>[^@]*)@)?(?<host>[^:/]+)(?::(?<port>\\d+))?(?:/(?<vhost>.*))?$",
 			boost::regex::icase
 		);
-		boost::smatch matches; 
-		if(!regex_match(uri, matches, re)) return false;
-		protocol_ = matches["protocol"];
-		host_ = matches["host"];
-		if(!std::string(matches["port"]).empty()) {
-			port_ = std::stoi(matches["port"]);
-		} else {
-			port_ = default_port();
-		}
-		user_ = matches["user"];
-		pass_ = matches["password"];
-		vhost_ = matches["vhost"];
-		return true;
+		boost::smatch matches;
+		if(!regex_match(uri, matches, re)) throw std::runtime_error("Failed to parse " + uri);
+		std::string proto = matches["protocol"];
+		std::string host = matches["host"];
+		uint16_t port = std::string(matches["port"]).empty() ? port_for_proto(proto) : std::stoi(matches["port"]);
+		std::string user = matches["user"];
+		std::string pass = matches["password"];
+		std::string vhost = matches["vhost"];
+		return connection_details(
+			host,
+			user,
+			pass,
+			vhost,
+			port,
+			proto
+		);
 	}
 
-	uint16_t default_port() const {
-		if(protocol_ == "amqp") return 5672;
-		if(protocol_ == "amqps") return 5671;
-		throw new std::runtime_error("Unknown AMQP scheme " + protocol_);
+	static uint16_t port_for_proto(const std::string &proto) {
+		if(proto == "amqp") return 5672;
+		if(proto == "amqps") return 5671;
+		throw std::runtime_error("Unknown AMQP scheme " + proto);
 	}
+
+	uint16_t default_port() const { return port_for_proto(protocol_); }
 
 	std::string host() const { return host_; }
 	std::string vhost() const { return vhost_; }
@@ -92,37 +105,37 @@ public:
 
 virtual ~client() { }
 
-	cps::future::ptr
+	std::shared_ptr<cps::leaf_future<std::shared_ptr<net::amqp::connection>>>
 	connect(
-		std::string host = "localhost",
-		uint16_t port = 5672
+		const connection_details &cd
 	)
 	{
 		using boost::asio::ip::tcp;
 		auto self = shared_from_this();
-		auto f = cps::future::create();
+		std::shared_ptr<cps::leaf_future<std::shared_ptr<net::amqp::connection>>>
+		 f = cps::leaf_future<std::shared_ptr<net::amqp::connection>>::create();
 
 		try {
 			auto resolver = std::make_shared<tcp::resolver>(service_);
 			auto query = std::make_shared<tcp::resolver::query>(
-				host,
-				std::to_string(port)	
+				cd.host(),
+				std::to_string(cd.port())
 			);
 			auto socket_ = std::make_shared<tcp::socket>(service_);
 			DEBUG << "Resolving RabbitMQ server";
 
 			resolver->async_resolve(
 				*query,
-				[query, socket_, resolver, f, host, port, self](boost::system::error_code ec, tcp::resolver::iterator ei) {
+				[query, socket_, resolver, f, cd, self](boost::system::error_code ec, tcp::resolver::iterator ei) {
 					if(ec) {
 						ERROR << "Error resolving AMQP server address: " << ec;
 						f->fail(ec.message());
 					} else {
-						DEBUG << "Connecting to AMQP server on " << host << ":" << port;
+						DEBUG << "Connecting to AMQP server on " << cd.host() << ":" << cd.port();
 						boost::asio::async_connect(
 							*socket_,
 							ei,
-							[socket_, f, host, port, self](boost::system::error_code ec, tcp::resolver::iterator it) {
+							[socket_, f, cd, self](boost::system::error_code ec, tcp::resolver::iterator it) {
 								if(ec) {
 									ERROR << "Had error: " << ec;
 									f->fail(ec.message());
@@ -130,7 +143,9 @@ virtual ~client() { }
 									DEBUG << "Connected to AMQP server";
 									tcp::socket::non_blocking_io nb(true);
 									socket_->io_control(nb);
-									f->done();
+									auto mc = std::make_shared<net::amqp::connection>(socket_, self->service_);
+									mc->setup();
+									f->done(mc);
 								}
 							}
 						);
@@ -151,25 +166,13 @@ virtual ~client() { }
 	}
 
 private:
-    boost::asio::io_service &service_;
+	boost::asio::io_service &service_;
 };
 
 };
 };
 
 namespace std {
-string to_string(const net::amqp::connection_details &details)
-{
-	string s = details.protocol() + "://";
-	if(details.have_auth()) {
-		s += details.username() + ":" + details.password() + "@";
-	}
-	s += details.host();
-	if(details.have_port()) {
-		s += ":" + to_string(details.port());
-	}
-	s += "/" + details.vhost();
-	return s;
-}
+	string to_string(const net::amqp::connection_details &details);
 };
 
