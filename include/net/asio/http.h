bf01f79fab69ea2fb4d5715e439fde8cbc20c690
Tom Molesworth <tom@perlsite.co.uk>
2015-06-07 22:07:54 +0100

    Server/connection/client notes

--- include/net/asio/http.h
+++ include/net/asio/http.h
@@ -10,19 +10,6 @@ namespace net {
 namespace http {
 
 /**
- * We may have zero or more connections to the same host information.
- * Host information is defined by any TLS-related data - client certificate,
- * for example - and host. Note that we treat each host as a separate
- * endpoint even if it happens to resolve an existing IP address: this is
- * to improve locality when using HTTP/2.
- *
- * It may be a design error for some applications. Please complain if you
- * think this is the case.
- */
-class connection { };
-class client { };
-
-/**
  * Represents a single header in a request or ressponse
  */
 class header : std::pair<std::string, std::string> {
@@ -353,6 +340,36 @@ protected:
 	/** e.g. 'OK' */
 	std::string status_message_;
 };
+
+/**
+ * We may have zero or more connections to the same host information.
+ * Host information is defined by any TLS-related data - client certificate,
+ * for example - and host. Note that we treat each host as a separate
+ * endpoint even if it happens to resolve an existing IP address: this is
+ * to improve locality when using HTTP/2.
+ *
+ * It may be a design error for some applications. Please complain if you
+ * think this is the case.
+ *
+ * The base connection class holds a socket - typically either plain TCP
+ * or TLS-over-TCP.
+ */
+class connection { };
+
+/**
+ * The client class owns all outgoing connections and is responsible for establishing
+ * new ones as necessary.
+ *
+ * As each request completes, a connection will be released back to the client.
+ * This connection is then available to be passed out to the next request.
+ */
+class client { };
+
+/**
+ * The server class owns all incoming connections and is responsible for accepting
+ * new ones as necessary.
+ */
+class server { };
 
 };
 
